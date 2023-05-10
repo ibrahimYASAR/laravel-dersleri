17725448cd26418880092984601421ca5616e4d3
ibrahimYASAR <100451756+ibrahimYASAR@users.noreply.github.com>
2023-05-10 06:05:56 +0300

    first commit

---
+++ database/migrations/timestamp/_create_chirps_table.php
@@ -0,0 +1,18 @@
+<?php
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chirps', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('message');
+            $table->timestamps();
+        });
+    }
+ 
+};
